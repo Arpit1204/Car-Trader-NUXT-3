da32e29f7a404b54bd4dd92e82497dfd24dde08e
Arpit Gulati <gulati.arpit0055@gmail.com>
2023-06-12 19:24:38 +0530

    Nuxt

--- components/Car/Detail/Contact.vue
+++ components/Car/Detail/Contact.vue
@@ -9,7 +9,7 @@
       <textarea class="border p-1 w-full" placeholder="Message"></textarea>
     </div>
     <button class="bg-blue-400 text-white px-10 py-3 rounded mt-4">
-      Submit
+      Submit!!!
     </button>
   </div>
 </template>
--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,16 +1,23 @@
 <template>
-  <div>
-    <CarDetailHero />
-    <CarDetailAttributes />
-    <CarDetailDescription />
+  <div v-if="car">
+    <CarDetailHero :car="car"/>
+    <CarDetailAttributes :features="car.features"/>
+    <CarDetailDescription :description="car.description" />
     <CarDetailContact />
   </div>
 </template>
 
 <script setup>
+
 const route = useRoute();
 const {toTitleCase}  = useUtilities()
+const {cars} = useCars()
 
+const car = computed(()=>{
+return cars.find((e)=>{
+  return e.id === parseInt(route.params.id)
+})
+})
 useHead({
   title: `${toTitleCase(route.params.name)}`,
 });
